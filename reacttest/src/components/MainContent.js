10984a06f3c3ae384366373c53ed47641adf0125
sarojregmi926 <sarojregmi926@gmail.com>
2024-05-02 22:18:43 -0400

    Welcome added 1 to 4

--- reacttest/src/components/MainContent.js
+++ reacttest/src/components/MainContent.js
@@ -5,7 +5,10 @@ function MainContent() {
   return (
     <main>
       <h2>This is the main content</h2>
-      <p>Welcome to our website!</p>
+      <p>Welcome to our website!1</p>
+      <p>Welcome to our website!2</p>
+      <p>Welcome to our website!3</p>
+      <p>Welcome to our website!4</p>
     </main>
   );
 }
