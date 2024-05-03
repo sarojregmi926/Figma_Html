11b6789ec9fa4b6ecb0c8e4b65cb52593065b934
sarojregmi926 <161094030+sarojregmi926@users.noreply.github.com>
2024-05-02 22:17:52 -0400

    Update MainContent.js

--- reacttest/src/components/MainContent.js
+++ reacttest/src/components/MainContent.js
@@ -6,6 +6,8 @@ function MainContent() {
     <main>
       <h2>This is the main content</h2>
       <p>Welcome to our website!</p>
+      <h2>This is change in remote</h2>
+    
     </main>
   );
 }
